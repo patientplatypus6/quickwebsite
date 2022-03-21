112cfaf63b70b0375822f80d9ea1b965b8a853dd
Peter Weyand <peterweyand@Peters-MacBook-Pro.local>
2022-03-21 11:02:12 -0400

    before server push

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    Welcome to Lightchan.
+  </div>
+  <img src="http://localhost/media/pics/lightbulb.jpeg"/>
+</template>
+
+
+<script scoped>
+    import app from '../App.vue'
+    export default{
+        components:{
+            app
+        }, 
+        data(){
+
+        }
+    }
+</script>
